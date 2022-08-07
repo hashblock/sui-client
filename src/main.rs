c1dc0294e174a05cd3f5a140e26efe8520239e78
Frank V. Castellucci <frankiecast@gmail.com>
2022-08-07 04:21:59 -0400

    Use local wallet

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::str::FromStr;
+use sui::client_commands::WalletContext;
+use sui_config::{sui_config_dir, SUI_CLIENT_CONFIG};
 use sui_sdk::types::base_types::SuiAddress;
-use sui_sdk::SuiClient;
+
+/// Get the total balance of gas for Sui address
+async fn get_total_gas_balance(
+    wallet: &WalletContext,
+    address: SuiAddress,
+) -> Result<u64, anyhow::Error> {
+    let balances = wallet.gas_objects(address).await?;
+    let mut total_balance = 0u64;
+    for gas in balances {
+        total_balance = total_balance + gas.0;
+    }
+    Ok(total_balance)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let sui = SuiClient::new_http_client("https://gateway.devnet.sui.io:443")?;
-    let address = SuiAddress::from_str("0x7f8e8f419d39ef61ca63b5d474084d1cd8b4e4f0")?;
-    let objects = sui.get_objects_owned_by_address(address).await?;
-    for sui_object in objects {
-        println!("{:?}", sui_object);
-    }
-    // println!("{:?}", objects);
+    // Instantiate wallet context
+    let mut config_path = sui_config_dir()?;
+    config_path.push(SUI_CLIENT_CONFIG);
+    let wallet = WalletContext::new(&config_path).await?;
+
+    // Get total gas balance
+    println!(
+        "Total balance {}",
+        get_total_gas_balance(&wallet, wallet.config.active_address.unwrap()).await?
+    );
     Ok(())
 }
